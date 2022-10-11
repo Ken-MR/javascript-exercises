cdb8979f77441385bd231332766f772c145bb64f
KenMR <kmr5591@gmail.com>
2022-10-11 19:36:47 -0400

    Add solutions for all tests except more than two arguments

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(text, item, item2 = undefined) {
+    let index = text.indexOf(item);
+    if (index > -1) {
+        text.splice(index, 1);
+    }
+    let index2 = -1;
+    if (!(item2 === undefined)) {
+        index2 = text.indexOf(item2);
+    }
+    if ((index2 > -1)) {
+        text.splice(index2, 1);
+    }
+    return text;
 };
 
 // Do not edit below this line
