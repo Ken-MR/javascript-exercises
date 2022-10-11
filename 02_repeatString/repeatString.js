7deefcd0a50dd85ca3e1ecd8e7a6637cfd9712d6
KenMR <kmr5591@gmail.com>
2022-10-10 23:25:40 -0400

    Add repeat string exercise solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(text, num) {
+    let stringOutput = '';
+    if (num < 0) {
+        return 'ERROR'
+    }
+    for (i = 0; i < num; i++) {
+        stringOutput += text;
+    }
+    return stringOutput;
 };
 
 // Do not edit below this line
