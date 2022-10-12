341451dafae0df40dcb2c4f70f1668ef991fd6f3
KenMR <kmr5591@gmail.com>
2022-10-11 20:49:15 -0400

    Add full solution for remove from array exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,14 +1,10 @@
-const removeFromArray = function(text, item, item2 = undefined) {
-    let index = text.indexOf(item);
-    if (index > -1) {
-        text.splice(index, 1);
-    }
-    let index2 = -1;
-    if (!(item2 === undefined)) {
-        index2 = text.indexOf(item2);
-    }
-    if ((index2 > -1)) {
-        text.splice(index2, 1);
+const removeFromArray = function(text) {
+    for (let i = 1; i < arguments.length; i++) {
+        for (let j = text.length - 1; j >= 0; j--) {
+            if((arguments[i] === text[j])){
+                text.splice(j, 1);
+            }
+        }
     }
     return text;
 };
