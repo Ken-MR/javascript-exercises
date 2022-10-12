621b59430b10ab733a7f935c70e12cc7df62db26
KenMR <kmr5591@gmail.com>
2022-10-11 20:26:00 -0400

    Add leap year exercise solution

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let centuryYear;
+    let century_400 = 0;
+    if (year % 100 === 0) {
+        centuryYear = year / 100;
+        century_400 = centuryYear % 4;
+    }
+    if ((year % 4 === 0) && !century_400) {
+        return true
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
